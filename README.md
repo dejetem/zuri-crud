0066bc0e78f4b657578eec2826dd78778a6d9101
dejetem <enyeoluwarotimisimon@gmail.com>
2021-04-27 04:44:15 +0100

    added some functonalities

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # zuri-crud
+
+## Getting Started the first thing to do is to clone the repository:
+
+$ git clone https://github.com/dejetem/zuri-crud.git
+$ cd zuri-crud
+
+## Create a virtual environment to install dependencies in and activate it:
+
+```bash
+$ python -m venv env
+$ env\Scripts\activate.bat
+```
+
+## Then install the dependencies:
+
+```bash
+(env)$ pip install -r requirements.txt
+```
+
+Note the (env) in front of the prompt. This indicates that this terminal session operates in a virtual environment set up by venv
+
+## run the server:
+
+```bash
+(env)$ python manage.py runserver
+```
+
+
+# Tests
+
+## To run the tests
+
+```bash
+(env)$ python manage.py test blog
+```
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,43 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@gmail.com',
+            password='test123'
+        )
+
+        self.post = Post.objects.create(
+            title='test title',
+            body='test body ✈ 😻, this on point',
+            author=self.user
+        )
+
+    def test_string_representation(self):
+        post = Post(title='test sample title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A better title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}',  'A bette body for better title')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice yooo')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Nice title')
+        self.assertTemplateUsed(response, 'post_detail.html')
